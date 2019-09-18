99d61e337441e11ea2056bf443b207442c37f013
Patrik Lundin <patrik@nothisispatrik.com>
2019-09-17 21:30:24 -0700

    Some prettying up and some comments

--- st_bin.py
+++ st_bin.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 import time
 
+__author__ = "Patrik Lundin" 
+__copyright__ = "Copyright 2018, nothisispatrik.com" 
+__license__ = "LGPL v3. Algos are whatever SHA1/HMAC/TOTP are, code is 100% from scratch"
+__email__ = "patrik@nothisispatrik.com"
+__status__ = "Prototype"
+__doc__ = """A reimplementation of TOTP, like the thing Google Authenticator et al uses. 
+Do not rely on this for security. Reimplementing primitives like this isn't safe, or at least not nearly as safe as using proper audited ones.
+This is a pre-version that works, keeps a pure immutable function flow from secret + time to output.
+It doesn't, however, simulate the whole bit and calc thing with strings like the other version - just
+normal python primitives here. It's quite a bit easier to follow that way.
+"""
+
+
+    # SHA1, with a few "forced" parts for sizes
 def H(m):
         # need rotate left (primitives only has shifts)
     rol = lambda n,b:((n<<b)|(n>>(32-b)))&0xffffffff
@@ -12,10 +26,9 @@ def H(m):
         (len(m)*32) if i==31 else       # Message lenth in bits as int32 last
         0)                              # Rest is zeros
 
-        # expanded key pos -> key.
+        # expanded key pos -> key. mapping
     w = lambda k,i: k(i) if i<16 else rol(w(k,i-3)^w(k,i-8)^w(k,i-14)^w(k,i-16),1)
-        # sha1 80 round loop, tail recurse
-    ra = lambda L,k,i:(C(ra(L,k,i-1), k, i) if i else C(L, k, i))
+
 
         # inner loop. 
     C = (lambda S,k,i:((rol(S[0],5) + (
@@ -27,11 +40,15 @@ def H(m):
                     ( 0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xCA62C1D6)[i//20] + 
                     w(k,i)&0xffffffff),S[0],rol(S[1],30),S[2],S[3]))
 
-        # Accumilator 
+        # sha1 80 round block loop, tail recursive
+    ra = lambda L,k,i:(C(ra(L,k,i-1), k, i) if i else C(L, k, i))
+
+        # Accumilator for adding, rather than replacing, the state @ new block
     ladd = lambda L,k: (
         (lambda P,Q:tuple( (x+y)&0xffffffff for (x,y) in zip(P,Q) ))(
             L, ra(L,k,79) ))
 
+        # pass the function chain that maps top the input and the SHA1 init vector and we're off
     return list(ladd(ladd(
                 (0x67452301,0xefcdaB89,0x98Badcfe,0x10325476,0xc3d2e1f0), mf)
                 ,lambda i:mf(i+16))) 
@@ -44,9 +61,16 @@ def ntotp(s, t):
         bM = bM[:-(len(bM)%8)]
 
     bM = bM + "0"*(512-len(bM))
+        
+        # You may note that I strangely *do* use ascii '0'/'1' bit strings here.
+        # It's merely a convenient/compact way to do base32 decode - the reduced alphabet
+        # puts out 5 bits per char, i.e. replaces 8 chars with 5 bytes. Awkward, yo.. 
+        # With so little data, it's be nearly unrolled by itself. So, given the little data,
+        # I just unpack it to 5 bits, pile it all together, and split it back to words.
 
     K = [int(bM[i:i+32],2) for i in range(0,len(bM),32)]
     M = [ 0, t//30 ]
+        # These two can be lambda:ed into the return too, just not totally feeling the benefit.
 
     return (lambda t:((t>>(160-32-(t&15)*8))&0x7fffffff)%10**6)(
         (lambda h:sum(h[i]<<((4-i)*32) for i in range(5)))(
@@ -84,7 +108,6 @@ def test_some():
             print(f"t{tn}: PASS")
 
 def main():
-     # OTP_KEY = "gq5edg4zut7kmuec"
      OTP_KEY = "G5JVITRXJFDTCRCP"
 
      t = time.time()
--- st_str.py
+++ st_str.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 import time
 
+__author__ = "Patrik Lundin" 
+__copyright__ = "Copyright 2018, nothisispatrik.com" 
+__license__ = "LGPL v3. Algos are whatever SHA1/HMAC/TOTP are, code is 100% from scratch"
+__email__ = "patrik@nothisispatrik.com"
+__status__ = "Prototype"
+__doc__ = """A reimplementation of TOTP, like the thing Google Authenticator et al uses. Do not rely on this for security. Reimplementing primitives like this isn't safe, or at least not nearly as safe as using proper audited ones.
+Started as a small reassemble from the crypto primitives, as the TOTP client lib I sometime use isn't in python 3. Then I wondered about perhaps implementing a bare-bones version without so many depenednecies. Once that worked, some parts do not feel like they could fully be safe (beyond that sha1 is dated), so I rearranged it into a functionally expressed version to have a unbroken chain of immutable stateless function calls from key+time -> number. Because it's hard to see the bits, I moved the math/bit primitives into calls working on ascii strings of "0" and "1". That's what this file is.
+"""
+
     # Do logic on string of '0' and '1'
 def bitxor(a,b): return {"00":"0", "01":"1", "10":"1", "11":"0"}[a+b]
 def bitor(a,b):  return {"00":"0", "01":"1", "10":"1", "11":"1"}[a+b]
@@ -44,14 +53,16 @@ def bH(bm):
         # Key expansion. As pure no-state function, this ends up being
         # huge - w(73) depends on w(70), which in turn on w(67),64,61...
         # w/ four branches ea. 
+
     #w = lambda k,i: (k(i) if i<16 else wrol(
     #    wxor(wxor(wxor(w(k,i-3),w(k,i-8)),w(k,i-14)),w(k,i-16)),1))
 
         # There's now a bypassable option to cache those. It makes it`
         # no longer stateless, but speeds it up from 1-2min to ~.3-.5 sec.
-        # So if nothing else, it's handy for debugging
-    cache_w = 0
+        # So if nothing else, it's handy for debugging.
+    cache_w = 1
       
+        # Little stateful cache wrapper
     ww = [-1]*80
     def sww(v,i):
         if not cache_w:
@@ -59,18 +70,23 @@ def bH(bm):
         if ww[i]==-1:
             ww[i] = v()
         return ww[i]
-
-        # like above, but with sww wrapper
+        
+        # map padded message to fully expanded message, given a function that produces the
+        # padded message and the postition (first 16 words copied, 16-80 a chain of xors of
+        # earlier ones). The parameterless lambda passed into sww will calculate it by recursing
+        # w() if called directly. sww will call it once for each i and cache it (or always if
+        # cache_w is 0)
+        
     w = lambda k,i:k(i) if i<16 else sww(lambda:wrol(
         wxor(wxor(wxor(w(k,i-3),w(k,i-8)),w(k,i-14)),w(k,i-16)),1),i)
 
-        # the 80 round sha1 loop, tail recursive
-    ra = lambda L,k,i:(C(ra(L,k,i-1), k, i) if i else C(L, k, i))
 
         # inside main loop. S is a five-word(int32) state, whose next
         # iteration is returned in parts ( lambda S:(a,b,c,d,e) ).
         # Combined wtih moving | ^ & + from infix to functions, it
-        # became rather opaque.
+        # became rather opaque, but it will return a new state form
+        # the prior state 'S', a function to access current round data 'k',
+        # and which sub-round this is 'i'.
        
     C = lambda S,k,i:(wadd(wadd(wadd(wadd(wrol(S[0],5) , (
         wxor(S[3],wand(S[1],wxor(S[2],S[3]))),
@@ -84,9 +100,30 @@ def bH(bm):
         '11001010011000101100000111010110')[i//20]), 
         w(k,i)), S[0], wrol(S[1],30), S[2], S[3])
 
-        # Rounds actually add to a set of five words, so here's a helper 
-        # function to deal with the accumilation.
-    ladd = lambda L,k: ((lambda P,Q: tuple( wand(wadd(x,y),wK(0xffffffff)) for (x,y) in zip(P,Q) ))(L,ra(L,k,79)))
+        # runs 'i' loops of C, recursively chainign them ( (C(C(C,k,0),k,1)....),k,79) ),
+        # like the normal loop would but as immutable chain instead of updated state
+    ra = lambda L,k,i:(C(ra(L,k,i-1), k, i) if i else C(L, k, i))
+
+        # Multiple loops (each doing a block) actually add the 80-round-grinded-out state
+        # to the prior state. So here's an accumilator func to do that, calls ra(), adds
+        # the prior state, and returns is, ready to chain to more blocks.
+
+        # The number of blocks is actually hardcoded to two here. Just from the HMAC spec,
+        # it be just one - it'd get hashed and padded so that it'd be two by here. I haven't
+        # seen anything saying it can't be three blocks, but most secrets are 16-20 bytes. 
+        # The average authenticator QR code encodes digits/periods too (which, granted, I ignore)
+        # looking something like:
+        # otpauth://totp/combined?secret=vvfmehlfxhmorcs2x2g67wgl6i65bxzl&algorithm=SHA1&digits=6&period=30
+        # (a 97 char payload). That would have to go up to 280ish to get a three blocker, which
+        # certainly doesn't violate the QR or url limits or anything, but it is a busy code/long url
+        # and I haven't seen one so far.
+        
+    ladd = lambda L,k: ((lambda P,Q: tuple( wadd(x,y) for (x,y) in zip(P,Q) ))(L,ra(L,k,79)))
+
+        # The 5x32 words here is the sha1 init vector, constant per spec. 'mf()' is where to get the
+        # data. I temp stoe in iR here rather than (more purely funcitonal) pass it to the next block
+        # because if we *are* cheating with the cache thing, it needs reset each block. Dropping it
+        # into the next ladd(.. works fine.
 
     iR = ladd( 
         ('01100111010001010010001100000001', 
@@ -97,27 +134,40 @@ def bH(bm):
 
     ww = [-1]*80 # flush cache, if there
 
+        # Second round. The finalizing-padding-digesting thing was just done with the inital pad/prep,
+        # easier that way we know up front there won't be more data suddenly showing up. To offset
+        # what to read in the input data, I just make a fresh function "lambda i:f(i+16)" to hit block
+        # two. If there were more blocks, we could keep iterating them by applyting this to itself
+        # for future blocks to keep shoving it 16x32=512 bits forward until it ran out.
     return list(ladd(iR, lambda i:mf(i+16))) 
 
 def ntotp(s, t):
-    B = ''.join(f"{'abcdefghijklmnopqrstuvwxyz234567'.index(c):05b}" for c in s if c!='=')
+        # Base32 decoder. Not completely flow-through here, since it's just massaging the data
+    B = ''.join(f"{'abcdefghijklmnopqrstuvwxyz234567'.index(c):05b}" for c in s.lower() if c!='=')
     if len(B)%8:
         B = B[:-(len(B)%8)]
 
+        # Pad
     B = B + "0"*(512-len(B))
+        # Break into 32 bit words, ready for hashign
     bK = [B[i:i+32] for i in range(0,len(B),32)]
-    """
-    M = [ 0, t//30 ]
-    bM = []
-    for a in M:
-        bM.append("0"*32, bin(int(t//30))[2:].zfill(32))
-    """
+
+        # Same format for the time, or actually number of intervals (default 30s) since the epoch,
+        # 64 bit bit width. I presume we're pre-2106 like a punk badass and just zero pad - sha1
+        # surely must be irrelevant by then..
+        
     bM = ["0"*32, bin(int(t//30))[2:].zfill(32)]
 
+        # HMAC will want an inner and outer key for it's thing, so I make them.
     biK = [wxor(a,"00110110"*4) for a in bK] # 0011(3) 0110(6)
     boK = [wxor(a,"01011100"*4) for a in bK] # 0101(5) 1100(c)
 
+        # hash the inner key concat the time, then the outer concat the result.
     bm = bH( boK + bH( biK + bM ) )
+        # Cleared HMAC and SHA1, time for TOTP again. Take the 4 LSBits from the result, offset
+        # into the whole result from the MSB by that many byte, get 32 bits from there. Then 
+        # clear the MSB on those 32 bits ([1::32] skips getting it) to ensure signed/unsigned 
+        # won't be a hassle. Convert to decimal, get the 6 LS..Ds? Yeah, least siginificant digits..
 
     bq = ''.join(bm)
     bq = bq[(((bq[-4]=='1')<<3)|
@@ -127,8 +177,15 @@ def ntotp(s, t):
     rvbq2 = int(bq,2)%(10**6) 
 
     return rvbq2 # Send them!
+        # This looks like I do a lot of tmp vars rather than piling the funcs together, and indeed
+        # I did. They do drop into each other just fine, but some of these do things that venture
+        # out of the ascii '01's box. Some of that is ok, but next step (to me) here is to start
+        # marking/differentiating/symbol-evaluating some of the bits, which will make some of it
+        # less fine again, and having them a little more spread out will be helpful. It doesn't *really*
+        # change anything since they're fairly explicitly not interacting between states.
 
-def testsome():
+    # Very arbitrary test values
+def testsome(): 
     t = 0  
     loud = 0 # Shout results during?
 
@@ -157,12 +214,15 @@ def testsome():
             print(f"t{tn}: FAIL {bin(fail)}")
         else:
             print(f"t{tn}: PASS")
+
 def main():
-    testsome()
-    quit()
+    #testsome()
+    #quit()
+            # if test/quit is commented out, run a TOTP round and present..
     s = 'vvfmehlfxhmorcs2x2g67wgl6i65bxzl'
     t = time.time()
-    print(ntotp(s,t))
+    print(f"One time code: {ntotp(s,t)}")
+    print(f"Changes in {30-t%30} seconds")
 
 if __name__=="__main__":
     main()
